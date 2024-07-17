0e19011ae8f8f7593c860d120b09ca5fd7e19ee8
Keynotseen89 <ksintora@yahoo.com>
2024-07-16 22:30:18 -0700

    updated jobFilter function

--- page-object-models/indeed-page-model/IndeedPage.ts
+++ page-object-models/indeed-page-model/IndeedPage.ts
@@ -8,6 +8,7 @@ export class IndeedPage {
     readonly searchInputBar: Locator;
     readonly inputJob: Locator;
     readonly inputLocation: Locator;
+    readonly searchButton: Locator;
     readonly filterDatePosted: Locator;
     readonly filterDatePostedMenu: Locator;
     readonly filterJobType: Locator;
@@ -24,56 +25,66 @@ export class IndeedPage {
         this.searchInputBar = this.page.locator("div[id=MosaicProviderRichSearchDaemon]").first();
         this.inputJob = this.searchInputBar.locator ("input[id=text-input-what]");
         this.inputLocation = this.searchInputBar.locator("input[id=text-input-where]");
-        this.filterDatePosted = this.searchInputBar.locator("button[id=filter-dateposted]");
-        this.filterDatePostedMenu = this.searchInputBar.locator("ul[id=filter-dateposted-menu]");
+        this.searchButton = this.searchInputBar.locator("button[type=submit]");
+        this.filterDatePosted = this.searchInputBar.locator("button[id=filter-dateposted]")
+        this.filterDatePostedMenu = this.searchInputBar.locator("ul[id=filter-dateposted-menu]")
         this.filterJobType = this.searchInputBar.locator("button[id=filter-jobtype]");
         this.filterJobTypeMenu = this.searchInputBar.locator ("ul[id=filter-jobtype-menu]");
         this.filterPayRate = this.searchInputBar.locator("button[id=filter-salary-estimate]");
         this.filterPayRateMenu = this.searchInputBar.locator ("ul[id=filter-salary-estimate-menu]");
-        this.filterLocation = this.searchInputBar.locator("id[filter-remotejob]");
-        this.filterLocationMenu = this.searchInputBar.locator("ul[filter-remotejob-menu]");
-        this.searchData = this.page.locator("div[id=mosaic-jobResults]").first();
-        this.dataCard = this.searchData.locator ("div[id=mosaic-provider-jobcards]").first();
+        this.filterLocation = this.searchInputBar.locator("button[id=filter-remotejob]");
+        this.filterLocationMenu = this.searchInputBar.locator("ul[id=filter-remotejob-menu]")
+        this.searchData = this.page.locator("div[id=mosaic-jobResults]")
+        this.dataCard = this.searchData.locator ("div[id=mosaic-provider-jobcards]")
     }
 
     async jobSearch(jobSearchString?: string, jobLocationString?: string){
         if(jobSearchString !== undefined){
             await this.inputJob.waitFor({ state: 'visible' })
             await this.inputJob.waitFor({ state: 'attached'})
-            await this.inputJob.click();
             await this.inputJob.fill(jobSearchString);
         }
         if(jobLocationString !== undefined){
             await this.inputLocation.waitFor({ state: 'visible'});
             await this.inputLocation.waitFor({ state: 'attached'});
-            await this.inputLocation.click();
             await this.inputJob.fill(jobLocationString);
         }else{
-            await this.inputLocation.click();
             await this.inputLocation.clear();
         }
     }
 
     async jobFilter(datePosted?: string, jobType?: string, jobLocation?: string, payRate?: string){
-        if(datePosted !== undefined && datePosted === "24 hours"){
+        if(datePosted !== undefined ){
+            await this.filterDatePosted.waitFor({ state: 'visible'});
+            await this.filterDatePosted.waitFor({ state: 'attached'});
             await this.filterDatePosted.click();
-            await this.filterDatePostedMenu.waitFor({ state: 'visible'})
-            await this.filterDatePostedMenu.locator(`li a, { hasText: 'Last 24 hours' }`).click();
+
+            await this.filterDatePostedMenu.evaluate(element => element.setAttribute('class', 'yosegi-FilterPill-dropdownList is-dropdownOpen'));
+            await this.filterDatePostedMenu.locator('li').locator('a:has-text("Last 24 hours")').click();
         }
-        if(jobType !== undefined && jobType === "Full-time"){
+        if(jobType !== undefined ){
+            await this.filterJobType.waitFor({ state: 'visible'});
+            await this.filterJobType.waitFor({ state: 'attached'});
             await this.filterJobType.click();
-            await this.filterJobTypeMenu.waitFor({ state: 'visible'})
-            await this.filterJobTypeMenu.locator(`li a, { hasText: 'Full-time'}`).click()
+
+            await this.filterJobTypeMenu.evaluate(element => element.setAttribute('class', 'yosegi-FilterPill-dropdownList is-dropdownOpen'));
+            await this.filterJobTypeMenu.locator('li').locator('a:has-text("Full-time")').click();
         }
-        if(jobLocation !== undefined && jobLocation === "Remote"){
-            await this.filterLocation.click();
+        if(jobLocation !== undefined){
             await this.filterLocation.waitFor({ state: 'visible'});
-            await this.filterLocationMenu.locator(`li a, { hasText: Remote}`).click();
+            await this.filterLocation.waitFor({ state: 'attached'});
+            await this.filterLocation.click();
+
+            await this.filterLocationMenu.evaluate(element => element.setAttribute('class', 'yosegi-FilterPill-dropdownList is-dropdownOpen'));
+            await this.filterLocationMenu.locator('li').locator('a:has-text("Remote")').click();
         }
-        if(payRate !== undefined && payRate === "80k"){
+        if(payRate !== undefined){
+            await this.filterPayRate.waitFor({ state: 'visible'});
+            await this.filterPayRate.waitFor({ state: 'attached'});
             await this.filterPayRate.click();
-            await this.filterPayRateMenu.waitFor({ state: 'visible'});
-            await this.filterPayRateMenu.locator(`li a, { hasText: $80,000+ }`).click();
+
+            await this.filterPayRateMenu.evaluate(element => element.setAttribute('class', 'yosegi-FilterPill-dropdownList is-dropdownOpen'));
+            await this.filterPayRateMenu.locator('li').locator('a:has-text("$80,000+")').click()
         }
     }
 
