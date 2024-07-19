26014e053493d82edc034b419cfba4af77f86e76
Keynotseen89 <ksintora@yahoo.com>
2024-07-18 23:58:08 -0700

    updated indeed_page

---
+++ url/page-url.ts
@@ -0,0 +1,8 @@
+/**
+ * Author: Quinatzin Sintora
+ * Date: 7/18/2024
+ */
+export enum PAGE_URL {
+    Indeed_Page = "https://indeed.com",
+    Craigslist_Page = "https://losangeles.craigslist.org/search/sof?bundleDuplicates=1&sort=date#search=1~list~0~0"
+}
